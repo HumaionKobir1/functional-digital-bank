f48cc366e218ad58cbd9fd8a52fd04bf98a85798
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-16 00:07:12 +0600

    Added => get and set element value using a function

--- deposit.js
+++ deposit.js
@@ -1,1 +1,20 @@
-console.log('Hi i am deposit');
+
+function depositBtn(){
+    // convert string value to a number
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+
+    // get previous deposit total by id
+    const previousDepositTotal = getElementValueById('deposit-total');
+
+    // calculate new deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+
+    // set deposit total value
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    // get previous balance by using the function
+    const previousBalanceTotal = getElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+
+}
--- utilities.js
+++ utilities.js
@@ -1,1 +1,19 @@
-console.log('i am utilities');
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+    // console.log(inputFieldValue);
+}
+function getElementValueById(elementId){
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElemetValue = parseFloat(textElementValueString);
+    return textElemetValue;
+}
+
+function setTextElementValueById (elementId, newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
